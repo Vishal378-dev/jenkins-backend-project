909911de7ff3e75b6e31b423abc2fa64379f93c1
Vishal378-dev <visahlsingh0512@gmail.com>
2026-02-22 21:44:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express=require('express');
 const app=express()
 
 app.get("/",(req,res)=>{
+    console.log("adding for checking new build")
     return res.status(200).json({
         success:true,
         message:"server is healthy"
